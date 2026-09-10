ab09a6093cd13e807a985d37063f50a039e139f6
wndmsrla0623 <wndmsrla0623@gmail.com>
2026-09-10 11:59:02 +0900

    jump map

---
+++ GEA_3D_1A/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public float jumpPower = 5f;
+    public float gravity = -20f;
+
+    private Vector2 moveInput;
+    private float verticalVelocity;
+    private CharacterController controller;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    // Update is called once per frame
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed && controller.isGrounded)
+        {
+            verticalVelocity = jumpPower;
+        }
+    }
+    void Update()
+    {
+        if (controller.isGrounded && verticalVelocity < 0f)
+        {
+            verticalVelocity = -2f;
+        }
+
+        verticalVelocity += gravity * Time.deltaTime;
+
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        move = move * moveSpeed;
+        move.y = verticalVelocity;
+
+        controller.Move(move * Time.deltaTime);
+    }
+} 
