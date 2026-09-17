8cfea266e5e499d95feea3637e202ee6f31ba686
wndmsrla0623 <wndmsrla0623@gmail.com>
2026-09-17 12:06:16 +0900

    복셀 캐릭터 및 벽

--- GEA_3D_1A/Assets/Scripts/PlayerController.cs
+++ GEA_3D_1A/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,18 @@ public class PlayerController : MonoBehaviour
     public float jumpPower = 5f;
     public float gravity = -20f;
 
+    public float mouseSensitivity = 0.2f;
+
+    private Vector2 lookInput;
+
+
+    public Transform cameraPivot;
+
+    public Transform cameraTransform;
+
+    private float pitch = 20f;
+
+
     private Vector2 moveInput;
     private float verticalVelocity;
     private CharacterController controller;
@@ -16,6 +28,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
 
@@ -32,8 +45,28 @@ public class PlayerController : MonoBehaviour
             verticalVelocity = jumpPower;
         }
     }
+
+    private bool isRunning;
+
+    public void OnSprint(InputValue value)
+    {
+        isRunning = value.isPressed;
+    }
+
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+    }
+    
+
     void Update()
     {
+        transform.Rotate(0f, lookInput.x * mouseSensitivity, 0f);
+
+        pitch = pitch - lookInput.y * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, -20f, 60f);
+        cameraPivot.localEulerAngles = new Vector3(pitch, 0f, 0f);
+
         if (controller.isGrounded && verticalVelocity < 0f)
         {
             verticalVelocity = -2f;
@@ -41,10 +74,24 @@ public class PlayerController : MonoBehaviour
 
         verticalVelocity += gravity * Time.deltaTime;
 
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-        move = move * moveSpeed;
+        Vector3 move = transform.forward * moveInput.y + transform.right * moveInput.x;
+
+        float speed = moveSpeed;
+        float targetZ = -6f;
+        if(isRunning)
+        {
+            speed = moveSpeed * 2f;
+            targetZ = -8f;
+        }
+        move = move * speed;
         move.y = verticalVelocity;
+
+        Vector3 camPos = cameraTransform.localPosition;
+        camPos.z = Mathf.Lerp(camPos.z, targetZ, 5f * Time.deltaTime);
+        cameraTransform.localPosition = camPos;
 
         controller.Move(move * Time.deltaTime);
     }
+
+    
 } 
